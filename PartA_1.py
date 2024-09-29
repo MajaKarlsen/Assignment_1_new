d2a066326b094d358b68a2848af04c50b013da41
MajaKarlsen <inger.karlsen@student.nhh.no>
2024-09-29 15:59:46 +0200

    Add files via upload

---
+++ PartA_1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 15:53:24 2024
+
+@author: inger
+"""
+#Part A, 1.
+
+from math import sqrt 
+
+def std_loops():
+#Define sum_a and sum_b
+    sum_a = 0
+    sum_b = 0
+
+#Define the list from the task
+    num_lst = [1,2,3,4,5]
+
+#Find the lenght of the list, so that i have a variable in the for-loop
+    N = len(num_lst)
+
+#Part 1
+    for i in range(1, N+1):
+        xi = num_lst[i - 1]
+        sum_a = sum_a + xi
+        
+    mx = 1 / N  * sum_a    
+
+#Part 2
+    for i in range(1, N+1):
+        b = (num_lst[i-1])**2
+        sum_b = sum_b + b
+
+    S = 1/N * sum_b
+
+#Part 3 and 4
+    o = sqrt(S - mx **2)
+
+    print(round(o,4))
+    
+std_loops()
